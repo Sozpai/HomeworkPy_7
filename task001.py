5d9358a0d38beb3d7ad08d03ed48a9eac345bd65
Aleksandr <lksndrszkn@yandex.ru>
2023-02-24 08:38:17 +0300

    added task

---
+++ task001.py
@@ -0,0 +1,14 @@
+# Задача 34
+line = "если-я-чушу в-затылке-не-беда"
+lines = line.split()
+ 
+print(lines)
+ 
+lst = [sum(x in 'уеыаоэяию' for x in lin)
+ for lin in lines]
+ 
+if len(set(lst)) == 1 :
+    res = "Парам пам-пам"  
+else: res = "Пам парам"
+ 
+print(res)
